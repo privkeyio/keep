15c15ff539539a909ea77a3f55ea41fdd78bbdfa
Kyle 🐆 <kyle@privkey.io>
2026-01-19 18:53:26 -0500

    fix: make header constants private in hidden module

--- keep-core/src/hidden/mod.rs
+++ keep-core/src/hidden/mod.rs
@@ -6,8 +6,7 @@
 //! This module provides encrypted storage with an optional hidden volume
 //! that is cryptographically indistinguishable from random data.
 
-pub mod header;
+mod header;
 pub mod volume;
 
-pub use header::{DATA_START_OFFSET, HEADER_SIZE, HIDDEN_HEADER_OFFSET};
 pub use volume::{HiddenStorage, VolumeType};
