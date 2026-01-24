1960913006e3ee963c7f2a3bc3fa5107c710d1f6
William K. Santiago <william@privkey.io>
2026-01-24 14:52:56 -0400

    Add SPDX license headers to keep-mobile

--- keep-mobile/build.rs
+++ keep-mobile/build.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: © 2026 PrivKey LLC
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 fn main() {
     uniffi::generate_scaffolding("src/keep_mobile.udl")
         .expect("failed to generate UniFFI scaffolding");
--- keep-mobile/src/lib.rs
+++ keep-mobile/src/lib.rs
@@ -1,1 +1,4 @@
+// SPDX-FileCopyrightText: © 2026 PrivKey LLC
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 uniffi::setup_scaffolding!();
